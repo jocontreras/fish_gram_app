64744473c0609725d27c48fc42f34253297058a4
jocontreras <jen.almarasy@gmail.com>
2017-07-08 15:44:57 +0200

    updating sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :exception
-  #before_action :authenticate_user!
+  protect_from_forgery with: :exception, unless: :json_request?
+
+  protected
+
+  def json_request?
+    request.format.json?
+  end
 end
