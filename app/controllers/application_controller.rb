87cb65fa9a88ada0e74c3e01c22e79ea817d1741
jocontreras <jen.almarasy@gmail.com>
2017-07-09 08:33:55 +0200

    adds and fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, unless: :json_request?
 
+  def authenticate_user!
+    authenticate_user_from_token!
+    super
+  end
+
+  def authenticate_user_from_token!
+    User.find_by_authentication_token(user_token)
+  end
+
+  def user_token
+    request.headers['X-AUTH-TOKEN'].presence || params['auth_token'].presence
+  end
+
   protected
 
   def json_request?
